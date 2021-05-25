169ca909cfad67895b37b02824f210b7b86234f6
a.gorbunova <gorbunova.work@gmail.com>
2021-05-25 11:06:06 +0300

    Верстка секции с видео.

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,22 @@
     <section-main />
 
     <section-characters />
+
+    <section-actions />
   </main>
 </template>
 
 <script>
 import SectionMain from './components/SectionMain/SectionMain.vue';
 import SectionCharacters from './components/SectionCharacters/SectionCharacters.vue';
+import SectionActions from './components/SectionActions/SectionActions.vue';
 
 export default {
   name: 'App',
   components: {
     SectionMain,
     SectionCharacters,
+    SectionActions,
   },
 };
 </script>
