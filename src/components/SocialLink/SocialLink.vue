e5c44bd9d7e09eaf5b4deba8c355e1669eb46daf
a.gorbunova <gorbunova.work@gmail.com>
2021-05-25 15:13:15 +0300

    Верстка секции «Футер».

---
+++ src/components/SocialLink/SocialLink.vue
@@ -0,0 +1,12 @@
+<template>
+<a class="social-link" :href="href"><slot /></a>
+</template>
+
+<script>
+export default {
+  name: 'social-link',
+  props: ['href'],
+};
+</script>
+
+<style lang="scss" src="./SocialLink.scss"/>
